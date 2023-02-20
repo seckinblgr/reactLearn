4a4356fa7f95f9c7e426dc881ecb136498afd4a7
Seçkin <secoblgr52@gmail.com>
2023-02-20 16:16:02 +0300

    first react projects

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2>Hello seçkin</h2>
+      <img src='./logo192.png'></img>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
